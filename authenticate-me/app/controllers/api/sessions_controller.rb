8fbe8f94f0852c3934337224d952a1df5e2f4da3
Frederick <fred.chien3@gmail.com>
2022-10-04 14:37:40 -0400

    add authenticate-me skeleton (BL)

---
+++ authenticate-me/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::SessionsController < ApplicationController
+  def show
+    # banana
+    if current_user
+      # render json: {user: current_user}
+      @user = current_user
+      render "api/users/show"
+    else
+      render json: {user: nil}
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:credential],
+      params[:password])
+    if @user
+      login!(@user)
+      # render json: {user: @user}
+      render "api/users/show"
+    else
+      render json: { errors: ['The provided credentials were invalid.'], status: :unauthorized }
+    end
+  end
+
+  def destroy
+    logout!
+    render json: { message: 'success' }
+  end
+end
+

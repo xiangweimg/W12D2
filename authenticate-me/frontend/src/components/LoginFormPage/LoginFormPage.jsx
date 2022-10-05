52e6853e85c41da84a3db4a3b3b8ae76f4ea7511
Xiangwei Liu <xiangweimg@gmail.com>
2022-10-05 09:30:39 -0400

    finishe errors

--- authenticate-me/frontend/src/components/LoginFormPage/LoginFormPage.jsx
+++ authenticate-me/frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { login } from "../../store/session"
+
 export default function LoginFormPage() {
     const [credential, setCredential] = useState("")
     const [password, setPassword] = useState("")
-    
+    const [validationErrors, setValidationErrors] = useState([])
+    const [hasSubmitted, setHasSubmitted] = useState(false)
+
     const dispatch = useDispatch()
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        setHasSubmitted(true)
+        if (validationErrors.length > 0) {alert("cannot submit")
+    } else {
         dispatch(login({credential,password}))
-    }
+        setCredential("")
+        setPassword("")
+        setHasSubmitted(false)
+    } 
+}
+
+    useEffect(()=> {
+        const errors = [];
+        if (credential.length < 1) errors.push("please enter your username or email")
+        if (password.length < 1) errors.push("please enter your password")
+        {setValidationErrors(errors)}
+    }, [credential, password])
+
     return (
-        <form onSubmit = {handleSubmit}>
+        <form onSubmit={handleSubmit}>
+            {hasSubmitted && validationErrors.length > 0 && (
+                    <div>
+                        The following errors were found:
+                        <ul>
+                            {validationErrors.map((error) => {
+                                return <li key={error}> {error} </li>
+                            })}
+                        </ul>
+                    </div>
+                )
+            }
+
             <label>username or email
                 <input type="text" 
                 value= {credential} 
